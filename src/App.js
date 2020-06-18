7b7b38791948250faf6b367234e8b8154c158a24
abhishek <abhishekpandey878@gmail.com>
2020-06-18 14:49:31 +0530

    Charts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chart from './components/Linechart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      chartData: {}
+    }
+  }
+
+  componentWillMount() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    this.setState({
+      chartData: {
+        labels: ['Mumbai', 'Pune', 'Hyderabad', 'Bangalore', 'Ahmedabad', 'Calcutta'],
+        datasets: [
+            {
+                label: 'Populations',
+                data: [
+                    617954, 181045, 153060, 106519, 105162, 95072
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(255, 159, 64, 0.6)',
+                    'rgba(255, 99, 132, 0.6)',
+                ]
+            }
+        ]
+    }
+    });
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <h2>React Charts</h2>
+        <Chart chartData = {this.state.chartData} />
+      </div>
+    );
+  }
 }
 
 export default App;
